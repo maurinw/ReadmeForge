c6a503d9e17a339348b9f0980b8ca2abc4fa1dd9
maurinw <dev.maurin@gmail.com>
2025-05-23 13:59:50 +0200

    init project

---
+++ src/main/java/ch/beginsecure/readmeforge/ReadmeforgeApplication.java
@@ -0,0 +1,13 @@
+package ch.beginsecure.readmeforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadmeforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadmeforgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/beginsecure/readmeforge/ReadmeforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.beginsecure.readmeforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadmeforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
